77b9b04c6b37c8edf0342e667d4482b47fc58880
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-07-15 01:43:42 -0400

    Updates

---
+++ precache-manifest.4deff658390b9b1a2adc51da918ab6cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01f717b89251507a45fde6601eec4bb",
+    "url": "/portfolio-2/index.html"
+  },
+  {
+    "revision": "add815d697c2c2b91421",
+    "url": "/portfolio-2/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "57c4968017cff9620463",
+    "url": "/portfolio-2/static/css/main.aca5c094.chunk.css"
+  },
+  {
+    "revision": "add815d697c2c2b91421",
+    "url": "/portfolio-2/static/js/2.d6cf70b2.chunk.js"
+  },
+  {
+    "revision": "57c4968017cff9620463",
+    "url": "/portfolio-2/static/js/main.1db570f8.chunk.js"
+  },
+  {
+    "revision": "d97e7774dc2181322827",
+    "url": "/portfolio-2/static/js/runtime~main.afccf5ec.js"
+  },
+  {
+    "revision": "fba6ab45bb6f591adb652e389f450ebf",
+    "url": "/portfolio-2/static/media/crystal-collector.fba6ab45.png"
+  },
+  {
+    "revision": "ddc28b43a52e1a073c264e79c38d4647",
+    "url": "/portfolio-2/static/media/remember-got.ddc28b43.png"
+  },
+  {
+    "revision": "22caeafd13be6ea0b6a07a35d6d4cbf4",
+    "url": "/portfolio-2/static/media/trickmember.22caeafd.png"
+  }
+]);
