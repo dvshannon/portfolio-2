a03e85ef5692a4d75a9e022ba6c2f6514da9f59a
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-07-17 19:48:28 -0400

    pathing

--- portfolio-july19/src/components/nav/Nav.js
+++ portfolio-july19/src/components/nav/Nav.js
@@ -18,12 +18,12 @@ function Nav () {
     }
     return (
         <div id="navbar">
-                <a href='https://github.com/dvshannon/' target="_blank" rel='noopener noreferrer'>Github</a>
-                <a href='https://www.linkedin.com/in/deyon-shannon-b911a9141/' target="_blank" rel='noopener noreferrer'>LinkedIn</a>
-                <span>Deyon Shannon</span>
-                <Link to='/'>Home</Link>
-                <Link to='/aboutme'>About Me</Link>
-            </div>
+            <a href='https://github.com/dvshannon/' target="_blank" rel='noopener noreferrer'>Github</a>
+            <a href='https://www.linkedin.com/in/deyon-shannon-b911a9141/' target="_blank" rel='noopener noreferrer'>LinkedIn</a>
+            <span>Deyon Shannon</span>
+            <Link to='/'>Home</Link>
+            <Link to='/aboutme'>About Me</Link>
+        </div>
     )
 }
 
