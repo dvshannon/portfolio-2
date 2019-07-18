8098e6868b0462db591b5593f69737a33967c311
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-07-18 00:05:00 -0400

    more path fixes

--- portfolio-july19/src/App.js
+++ portfolio-july19/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from './pages/landing/Landing';
-// import Projects from './pages/projects/Projects';
 import AboutMe from './pages/aboutme/AboutMe';
 import Nav from  './components/nav/Nav';
 
@@ -11,13 +10,13 @@ function App() {
   return (
       <Router>
         <Nav />
-          <div className="landing">
+          {/* <div className="landing"> */}
           <Switch>
             <Route exact path='/' component={Landing} />
             <Route exact path='/landing' component={Landing} />
             <Route exact path='/aboutme' component={AboutMe} />
           </Switch>
-        </div>
+        {/* </div> */}
       </Router>
   );
 }
